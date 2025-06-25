23281d91243e05903c96b16792783ed9b91acadb
dajeong01 <wpekwjd@naver.com>
2025-06-25 22:18:15 +0900

    userservice

---
+++ src/main/java/org/example/springboot_study_alone/controller/UsersController.java
@@ -0,0 +1,64 @@
+package org.example.springboot_study_alone.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.springboot_study_alone.dto.JwtDto;
+import org.example.springboot_study_alone.dto.LoginDto;
+import org.example.springboot_study_alone.dto.UserRegisterDto;
+import org.example.springboot_study_alone.security.service.JwtService;
+import org.example.springboot_study_alone.service.UsersService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+public class UsersController {
+    import com.korit.authstudy.dto.JwtDto;
+import com.korit.authstudy.dto.LoginDto;
+import com.korit.authstudy.dto.LoginDto;
+import com.korit.authstudy.dto.UserRegisterDto;
+import com.korit.authstudy.security.model.PrincipalUser;
+import com.korit.authstudy.security.service.JwtService;
+import com.korit.authstudy.service.UsersService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+    @Slf4j
+    @RestController
+    @RequestMapping("/api/users")
+    @RequiredArgsConstructor
+    public class UsersController {
+
+        private final UsersService usersService;
+        private final JwtService jwtService;
+        private LoginDto dto;
+
+        @PostMapping
+        public ResponseEntity<?> registerUser(@RequestBody UserRegisterDto dto) {
+            log.info("DTO: {}", dto);
+            return ResponseEntity.ok(usersService.register(dto));
+        }
+
+        @PostMapping("/login")
+        public ResponseEntity<?> login(@RequestBody LoginDto dto) {
+            JwtDto jwtDto = usersService.login(dto);
+            System.out.println("로그인 컨트롤러 호출");
+            return ResponseEntity.ok(jwtDto);
+        }
+
+        @GetMapping("/login/status")
+        public ResponseEntity<?> getLoginStatus(@RequestHeader("Authorization") String authorization) {
+            System.out.println(authorization);
+            return ResponseEntity.ok(jwtService.validLoginAccessToken(authorization));
+        }
+
+        @GetMapping("/principal")
+        public ResponseEntity<?> getPrincipalUser() {
+            return ResponseEntity.ok(SecurityContextHolder.getContext().getAuthentication());
+        }
+    }
+
+}
--- src/main/java/org/example/springboot_study_alone/service/UsersService.java
+++ src/main/java/org/example/springboot_study_alone/service/UsersService.java
@@ -1,21 +1,47 @@
 package org.example.springboot_study_alone.service;
 
+
+
 import lombok.RequiredArgsConstructor;
 import org.example.springboot_study_alone.domain.entity.User;
-import org.example.springboot_study_alone.dto.UserRegisterDto;
+import org.example.springboot_study_alone.dto.JwtDto;
+import org.example.springboot_study_alone.dto.LoginDto;
 import org.example.springboot_study_alone.repository.UsersRepository;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.example.springboot_study_alone.security.jwt.JwtUtil;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class UsersService {
 
+    private final BCryptPasswordEncoder passwordEncoder;
     private final UsersRepository usersRepository;
+    private final JwtUtil jwtUtil;
 
-    public User register(UserRegisterDto dto) {
+    public User register(org.example.springboot_study_alone.dto.UserRegisterDto dto) {
         User insertedUser = usersRepository.save(dto.toEntity(passwordEncoder));
         return insertedUser;
+    }
 
+    public JwtDto login(LoginDto dto) {
+        List<User> foundUsers = usersRepository.findByUsername(dto.getUsername());
+        if(foundUsers.isEmpty()){
+            throw new UsernameNotFoundException("사용자 정보(아이디를 못 찾음)를 확인하세요.");
+            // error 401
+            // 여기서 예외 터지면 바로 login 메서드 종료
+        }
+        User user = foundUsers.get(0);
+        if(!passwordEncoder.matches(dto.getPassword(), user.getPassword())){
+            throw new BadCredentialsException("사용자 정보(비밀번호 오류)를 확인하세요");
+        }
+        System.out.println("로그인 성공 토큰 생성");
+        String token = jwtUtil.generateAccessToken(user.getId().toString());
+        return JwtDto.builder().accessToken(token).build();
     }
 }
+
